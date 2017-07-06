f2dd758e72c77d7eae1aef8c33a52453b4db78f9
Kevin Sulonen <kevin.sulonen@gmail.com>
2017-07-06 11:09:44 -0700

    Routes + mongodb integration

--- routes/api_router.js
+++ routes/api_router.js
@@ -12,35 +12,68 @@ module.exports.router = router
   .get('/', async (ctx) => {
     let params = qs.parse(ctx.request.querystring);
     
-    if (Object.keys(params).length === 0) { 
-      ctx.body = 'All coupons';
+    if (Object.keys(params).length === 0) {
+      try {
+        ctx.body = await Coupon.find({});
+      } catch (err) {
+        ctx.status = 500;
+        ctx.body = err.stack;
+        console.error(err);
+      }
     }
     if (params.state) {
-      if (params.state == 'valid') {
-        ctx.body = 'Valid coupons';
-      } else if (params.state == 'invalid') {
-        ctx.body = 'Invalid coupons';
+      let now = Date.now();
+      if (params.state === 'valid') {
+        ctx.body = await Coupon.find({$where: 'Date.parse(this.expire_at) > ' + now});
+      } else if (params.state === 'invalid') {
+        ctx.body = await Coupon.find({$where: 'Date.parse(this.expire_at) <= ' + now});
       } else {
+        ctx.status = 400;
         ctx.body = 'Invalid parameter';
       }
     }
   })
   .get('/:couponId', async (ctx) => {
-    ctx.body = ctx.params.couponId;  
+    try {
+      if (ctx.params.couponId.length !== 24) {
+        ctx.status = 400;
+        ctx.body = 'Invalid parameter';
+      } else {
+        let result = await Coupon.find({_id: ctx.params.couponId});
+        console.log(result);
+        if (typeof result[0] === 'undefined') ctx.status = 404;
+        ctx.body = result;
+      }
+    } catch (err) {
+      ctx.status = 500;
+      ctx.body = err.stack;
+      console.error(err);
+    }
   })
   .post('/', async (ctx) => {
     let newCoupon = new Coupon(ctx.request.body);
-    await newCoupon.save((err, res) => {
-      if (err) {
-        console.error(err);
-        ctx.body = err;
-      }
-      ctx.body = 'New coupon added'; 
-    });
+    try {
+      await newCoupon.save();
+      ctx.body = newCoupon;
+    } catch (err) {
+      ctx.status = 500;
+      ctx.body = err.stack;
+      console.error(err);
+    }
   })
   .put('/:couponId', async (ctx) => {
-    // update coupon
-    ctx.body = ctx.params.couponId;  
+     try {
+    //   If invalid coupon ID or invalid payload, return HTTP status 400.
+    //   If non exist coupon ID, return HTTP status 404.
+    //   If no or invalid content type header, return HTTP status 415.
+    //   If server error, return HTTP status 500 with a reason payload.
+      ctx.body = await Coupon.findByIdAndUpdate(ctx.params.couponId, ctx.request.body, {new: true});
+      ctx.status = 200;
+    } catch (err) {
+      ctx.status = 500;
+      ctx.body = err.stack;
+      console.error(err);
+    }
   })
   .delete('/:couponId', async (ctx) => {
     // delete coupon
